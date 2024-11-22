8ef5034a71173093f36c859c8f87ce02f676d89d
clanie1 <danybarociov@gmail.com>
2024-11-21 18:22:18 -0800

    react router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import SignIn from "./views/SignIn";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <SignIn />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<SignIn />} />
+    </Routes>
+  </BrowserRouter>
 );
