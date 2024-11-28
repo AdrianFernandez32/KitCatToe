9b979b7d24b36d16519bb7cd9ad4f03d150e3837
clanie1 <danybarociov@gmail.com>
2024-11-28 12:48:24 -0800

    frontend deploy

--- index.ts
+++ index.ts
@@ -1,17 +1,32 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as resources from "@pulumi/azure-native/resources";
 import * as storage from "@pulumi/azure-native/storage";
+import * as azure from "@pulumi/azure-native";
 
 const resourceGroupName = "ProyectoFinal3";
 
 // Create an Azure resource (Storage Account)
 const storageAccount = new storage.StorageAccount("sa", {
-  // resourceGroupName: resourceGroup.name,
   resourceGroupName: resourceGroupName,
   sku: {
     name: storage.SkuName.Standard_LRS,
   },
   kind: storage.Kind.StorageV2,
+});
+
+const staticWebApp = new azure.web.StaticSite("reactStaticApp", {
+  resourceGroupName: resourceGroupName,
+  location: "centralus",
+  sku: {
+    name: "Free",
+    tier: "Free",
+  },
+  repositoryUrl: "https://github.com/Clanie1/KitCatToe",
+  branch: "main",
+  buildProperties: {
+    appLocation: "/frontend",
+    outputLocation: "build",
+  },
 });
 
 // Export the primary key of the Storage Account
