fb13509142b358650a4188600bceb7e48a22695b
clanie1 <danybarociov@gmail.com>
2024-11-28 19:55:26 -0800

    chakra ui new dependency

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,15 +4,18 @@ import "./index.css";
 import SignIn from "./views/SignIn";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Game from "./views/Game/Game";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <Routes>
-      <Route index element={<SignIn />} />
-      <Route path="/game" element={<Game />} />
-    </Routes>
+    <ChakraProvider>
+      <Routes>
+        <Route index element={<SignIn />} />
+        <Route path="/game" element={<Game />} />
+      </Routes>
+    </ChakraProvider>
   </BrowserRouter>
 );
