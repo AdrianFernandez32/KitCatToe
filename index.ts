a93e7ee5157469bf1078bf45ea0596d0e8ba2629
clanie1 <danybarociov@gmail.com>
2024-11-30 13:47:49 -0800

    repository key from env

--- index.ts
+++ index.ts
@@ -2,6 +2,9 @@ import * as pulumi from "@pulumi/pulumi";
 import * as resources from "@pulumi/azure-native/resources";
 import * as storage from "@pulumi/azure-native/storage";
 import * as azure from "@pulumi/azure-native";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const resourceGroupName = "ProyectoFinal3";
 
@@ -21,18 +24,20 @@ const staticWebApp = new azure.web.StaticSite("reactStaticApp", {
     name: "Free",
     tier: "Free",
   },
+  repositoryToken: process.env.GITHUB_TOKEN,
   repositoryUrl: "https://github.com/Clanie1/KitCatToe",
   branch: "main",
   buildProperties: {
     appLocation: "/frontend",
+    apiLocation: "",
     outputLocation: "build",
   },
 });
 
 // Export the primary key of the Storage Account
-const storageAccountKeys = storage.listStorageAccountKeysOutput({
-  resourceGroupName: resourceGroupName,
-  accountName: storageAccount.name,
-});
+// const storageAccountKeys = storage.listStorageAccountKeysOutput({
+//   resourceGroupName: resourceGroupName,
+//   accountName: storageAccount.name,
+// });
 
-export const primaryStorageKey = storageAccountKeys.keys[0].value;
+// export const primaryStorageKey = storageAccountKeys.keys[0].value;
