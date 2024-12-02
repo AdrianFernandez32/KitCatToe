436d7c5d25da1b3a9669a0d8f1e711404755f133
iso-bravo <isolettebravo@gmail.com>
2024-12-02 00:51:09 -0800

    Protected Routes in index for each path

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,18 +6,43 @@ import SignIn from "./views/SignIn";
 import Register from "./views/Register";
 import Lobby from "./views/Lobby";
 import EditProfile from "./views/EditProfile";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route index element={<SignIn />} />
-      <Route path="/game" element={<Game />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/lobby" element={<Lobby />} />
-      <Route path="/editprofile" element={<EditProfile />} />
-    </Routes>
-  </BrowserRouter>
+  <AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<SignIn />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/game"
+          element={
+            <ProtectedRoute>
+              <Game />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/lobby"
+          element={
+            <ProtectedRoute>
+              <Lobby />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/editprofile"
+          element={
+            <ProtectedRoute>
+              <EditProfile />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  </AuthProvider>
 );
