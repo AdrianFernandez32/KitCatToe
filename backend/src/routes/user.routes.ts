2b9cb8c4fc1ac7ac7a18bef472592552c63b169a
iso-bravo <isolettebravo@gmail.com>
2024-12-02 19:11:58 -0800

    BR7.0: User info route

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { registerUser, updateProfile } from "../controllers/user.controller";
+import { registerUser, updateProfile, getUserInfo  } from "../controllers/user.controller";
+import { verifyToken } from "../utils/auth.middleware";
 
 const router = Router();
 
 router.post("/register", registerUser);
 router.put("/:id/profile", updateProfile);
+router.get("/info", verifyToken, getUserInfo);
 
 export default router;
