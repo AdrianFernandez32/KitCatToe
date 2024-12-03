0e2a9ac47cadedb3cf8d3c0c5e488e9d1e113b6c
iso-bravo <isolettebravo@gmail.com>
2024-12-02 19:10:27 -0800

    BR7.0: getUserInfo function

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { createUser, getUserByEmail } from "../services/user.service";
-import { updateUserProfile } from "../services/user.service";
+import { updateUserProfile, getUserWins, getUserById  } from "../services/user.service";
 
 export const registerUser = async (
   req: Request,
@@ -58,6 +58,25 @@ export const updateProfile = async (
     }
   } catch (error) {
     console.error("Error updating profile:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getUserInfo = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const userId = (req as any).user.id;
+
+    const user = await getUserById(userId);
+    if (!user) {
+      res.status(404).json({ error: "User not found" });
+      return;
+    }
+
+    const wins = await getUserWins(userId);
+
+    res.status(200).json({ user: { ...user, wins } });
+  } catch (error) {
+    console.error("Error fetching user info:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
